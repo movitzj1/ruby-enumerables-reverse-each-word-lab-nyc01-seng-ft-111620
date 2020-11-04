1ac186823d195fa0c9c0951dffe8f953816772e9
Justin Movitz <movitzj1@montclair.edu>
2020-11-04 16:07:19 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   empty_string = ""
-  string.split.each do |word| #Reference #https://stackoverflow.com/questions/28649812/ruby-loops-and-classes-splitting-a-string-into-an-array-and-back-to-a-string-ag
+  string.split.each do |word| #Reference Kris #https://stackoverflow.com/questions/28649812/ruby-loops-and-classes-splitting-a-string-into-an-array-and-back-to-a-string-ag
     new_word = word.reverse
     empty_string << new_word + " "
   end
